11afa96ede3864019864c374647ad2f3a8ceeca6
Shivam Shukla <shivamps1300@gmail.com>
2023-04-21 03:08:53 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDW5yvyl67Qz6pknz7I7dZOINnv1csTiRM",
+    authDomain: "netflix-clone-822e9.firebaseapp.com",
+    projectId: "netflix-clone-822e9",
+    storageBucket: "netflix-clone-822e9.appspot.com",
+    messagingSenderId: "636454031755",
+    appId: "1:636454031755:web:5f4bfb2f24b8e90b6e1f8e"
+  };
